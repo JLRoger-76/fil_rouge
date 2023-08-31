aadd0596147e32024553417c3f63db56cbce83b8
jlroger <jean-paul.roger76@orange.fr>
2023-08-31 12:48:34 +0200

    install service&component

---
+++ src/app/teacher/teacher.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TeacherFormComponent } from './teacher-form/teacher-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    TeacherFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TeacherModule { }
