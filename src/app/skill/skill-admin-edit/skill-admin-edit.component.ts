2bc333b4a2353975768ac2db90ff6ff33cbf05ea
jlroger <jean-paul.roger76@orange.fr>
2023-09-02 18:50:37 +0200

    modifie skill module

---
+++ src/app/skill/skill-admin-edit/skill-admin-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-admin-edit',
+  templateUrl: './skill-admin-edit.component.html',
+  styleUrls: ['./skill-admin-edit.component.css']
+})
+export class SkillAdminEditComponent {
+
+}
