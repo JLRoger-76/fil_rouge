b42e2b76e38d55c73a69f4f977b21e89cc04999f
jlroger <jean-paul.roger76@orange.fr>
2023-09-13 14:00:35 +0200

    maj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,12 @@ import { FormationModule } from './formation/formation.module';
 import { ThemeAdminComponent } from './theme/theme-admin/theme-admin.component';
 import { SkillModule } from './skill/skill.module';
 import { SessionModule } from './session/session.module';
-import { NavbarModule } from "./navbar/navbar.module";
+import { NavbarModule } from './navbar/navbar.module';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegistrationModule } from './registration/registration.module';
 import { RouterModule } from '@angular/router';
-import { PresenceComponent } from './registration/presence/presence.component';
+
 import { PresenceModule } from './registration/presence/presence/presence.module';
 import { EvaluationFormModule } from './evaluation-form/evaluation-form.module';
 import { InstructorModule } from './instructor/instructor.module';
@@ -36,6 +36,9 @@ import { InstructorModule } from './instructor/instructor.module';
         RegistrationModule,
         RouterModule,
         PresenceModule,
+        EvaluationFormModule,
+        ReactiveFormsModule,
         NavbarModule
+    ]
 })
 export class AppModule {}
