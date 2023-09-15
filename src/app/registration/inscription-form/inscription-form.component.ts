eb6374d1dc5efad43a5757bbef10dd316f8c58d8
lolo <lolo@gmail.com>
2023-09-15 16:35:49 +0200

    connexion back

--- src/app/registration/inscription-form/inscription-form.component.ts
+++ src/app/registration/inscription-form/inscription-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { RegistrationService } from '../registration.service';
 
@@ -21,7 +21,11 @@ export class InscriptionFormComponent implements OnInit {
   FormA = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
-    email: new FormControl(''),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+    ]),
     password: new FormControl(''),
     address: new FormControl(''),
 
